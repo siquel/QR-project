d94d98297c58235235d1014f4385c475f3c65d0d
siquel <squual@kahvipaussi.net>
2014-12-30 04:11:01 +0200

    Styled header

---
+++ app/views/partials/header.blade.php
@@ -0,0 +1,7 @@
+<a href="#" id="header_open"><span>B</span>Menu</a>
+<div id="header">
+    <a href="#"><span>@</span>Home</a>
+    <a href="#"><span>.</span>Profile</a>
+    <a href="#"><span>;</span><em>1</em>Events</a>
+    <a href="#"><span>,</span>Logout</a>
+</div>

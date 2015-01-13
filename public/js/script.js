5d6a4fe90edc3a783d68966768b8ffb85287c55a
siquel <squual@kahvipaussi.net>
2015-01-13 07:07:46 +0200

    Responsive view fixes for calendar, some JS shit for sorting, gotta replace with ajax soon

--- public/js/script.js
+++ public/js/script.js
@@ -7,42 +7,67 @@ $(function() {
         $('#sub_menu').toggleClass('sub_open');
     });
 
-    var cal = $('#calendar').fullCalendar({
-        buttonText: {
-            prev: '&#210;',
-            next: '&#213;'
-        },
-        header: {
-            left: 'prev',
-            center: 'title',
-            right: 'next'
-        },
-        firstDay: 1 // monday
-    });
 
-    $.fn.getMenu = function() {
-        $('#sub_menu a').remove();
-
-        var curMonth = cal.fullCalendar('getDate').getMonth()+1;
-        var curYear = cal.fullCalendar('getDate').getFullYear();
-
-        var found = $.grep(file, function(obj) {
-            return obj.menu === ""+curMonth+curYear+"";
+    if ($('html').attr('id') == 'events') {
+        var cal = $('#calendar').fullCalendar({
+            buttonText: {
+                prev: '&#210;',
+                next: '&#213;'
+            },
+            header: {
+                left: 'prev',
+                center: 'title',
+                right: 'next'
+            },
+            firstDay: 1 // monday
         });
 
-        $.each(found, function(i, obj) {
-            $('#sub_menu').append('<a href="'+obj.url+'"><strong>'+obj.title+' '+obj.start.getDate()+'-'+curMonth+'-'+curYear+'</strong>'+obj.description+'</a>');
+        $.fn.getMenu = function() {
+            $('#sub_menu a').remove();
+
+            var curMonth = cal.fullCalendar('getDate').getMonth()+1;
+            var curYear = cal.fullCalendar('getDate').getFullYear();
+            var monthStr = curMonth.toString().length == 2 ? curMonth : '0'+curMonth;
+            var found = $.grep(file, function(obj) {
+                return obj.menu === ""+curMonth+curYear+"";
+            });
+
+            found.sort(function(a, b) {
+                return a.start.getDate() - b.start.getDate();
+            });
+
+            $.each(found, function(i, obj) {
+                var date = obj.start.getDate();
+                var dateStr = date.toString().length == 2 ? date : '0'+date;
+                $('#sub_menu').append('<a href="'+obj.url+'"><strong>'+obj.title+' '+dateStr+'-'+monthStr+'-'+curYear+'</strong>'+obj.description+'</a>');
+            });
+        }
+
+        $.fn.windowCheck = function() {
+            var ww = $(window).width();
+            if (ww > 650) {
+                cal.fullCalendar('changeView', 'month');
+            } else if (ww < 650 && ww > 400) {
+                cal.fullCalendar('changeView', 'basicWeek');
+            } else {
+                cal.fullCalendar('changeView', 'basicDay');
+            }
+        }
+
+        $(window).on('resize', function() {
+            $().windowCheck();
+        });
+
+        var file;
+        $.getJSON('api/events.php', function(data) {
+            file = data;
+            cal.fullCalendar('addEventSource', data);
+            $().getMenu();
+            $().windowCheck();
+        });
+
+        $('.fc-button-prev, .fc-button-next').on('click', function() {
+            $().getMenu();
         });
     }
-
-    var file;
-    $.getJSON('api/events.php', function(data) {
-        file = data;
-        cal.fullCalendar('addEventSource', data);
-        $().getMenu();
-    });
-
-    $('.fc-button-prev, .fc-button-next').on('click', function() {
-        $().getMenu();
-    });
 });
